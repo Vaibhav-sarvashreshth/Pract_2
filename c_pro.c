0140c7e956b656490881c0949f2cf280b79d87c1
Vaibhav-sarvashreshth <mokalevaibhav@gmail.com>
2022-02-18 20:38:47 +0530

    New c file added

---
+++ c_pro.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+#define square(a) (a)*(a)
+int main()
+{	
+
+	int a,b,i,n;
+	int arr[100];
+	
+	printf("\n Please enter the value of a : \n ");
+	scanf("%d",&a);
+	printf("\n The square of  a given number is %d \n",square(a));
+	
+	
+  	return 0;
+}
